958f02d3ea9868914eda4fdaab4677fc950fc026
jaehan1346 <jaehan1346@gmail.com>
2024-01-05 13:59:45 +0900

    update: 로깅 관련 설정 분리, 메일 관련 Bean 설정을 위한 MailConfig 추가

---
+++ server/src/main/java/com/example/demo/config/MailConfig.java
@@ -0,0 +1,39 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Profile("mail")
+@Configuration
+public class MailConfig {
+    private final Environment env;
+
+    public MailConfig(Environment env) {
+        this.env = env;
+    }
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(env.getProperty("spring.mail.host"));
+        mailSender.setPort(Integer.parseInt(env.getProperty("spring.mail.port")));
+        mailSender.setUsername(env.getProperty("spring.mail.username"));
+        mailSender.setPassword(env.getProperty("spring.mail.password"));
+
+        mailSender.setDefaultEncoding("utf-8");
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", env.getProperty("spring.mail.protocol"));
+        props.put("mail.smtp.auth", env.getProperty("spring.mail.properties.mail.smtp.auth"));
+        props.put("mail.smtp.starttls.enable", env.getProperty("spring.mail.properties.mail.smtp.starttls.enable"));
+        props.put("mail.debug", "true");
+
+        return mailSender;
+    }
+}
