decadd47ee249cce7d3e90368f9744c17de709c4
jaehan1346 <jaehan1346@gmail.com>
2024-01-05 15:22:15 +0900

    create: LocalDate, LocalDateTime에 사용할 DateTime PATTERN 관련 상수 클래스 추가

---
+++ server/src/main/java/com/example/demo/constants/DateTimeConst.java
@@ -0,0 +1,6 @@
+package com.example.demo.constants;
+
+public class DateTimeConst {
+    public static final String DATETIME = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATE = "yyyy-MM-dd";
+}
