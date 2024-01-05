58b71560b31080e15700bf5ae025e712dbe3bd97
jaehan1346 <jaehan1346@gmail.com>
2024-01-05 13:24:27 +0900

    update: WebConfig 파일에 EnableWebMvc 설정 추가

--- server/src/main/java/com/example/demo/config/WebConfig.java
+++ server/src/main/java/com/example/demo/config/WebConfig.java
@@ -3,9 +3,11 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     // Spring MVC Default CORS Settings
